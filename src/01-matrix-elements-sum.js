07958d0d62d518b6be1999d2b7f1bca1d853aef3
Iaroslav Silkin <maxfri1995@gmail.com>
2021-04-07 13:19:40 +0300

    feat: add solution for task 01

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -17,11 +17,10 @@
 function getMatrixElementsSum(matrix) {
   let sum = 0;
   for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[0].length; j++) {
-      if (matrix[i][j] === 0 && (i + 1) < matrix.length) {
-        sum += 0;
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i === 0 || matrix[i - 1][j] !== 0) {
+        sum += matrix[i][j];
       }
-      sum += matrix[i][j];
     }
   }
   return sum;
