51b1f621b72174e2e64e9c0edd0ccc1472fc0769
maxfri <maxfri1995@gmail.com>
2021-04-12 01:37:23 +0300

    feat: add solution for task 15-get-email-domain

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,4 @@
-// const ListNode = require('../extensions/list-node');
+const ListNode = require('../extensions/list-node');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
@@ -13,19 +13,38 @@
 
 class Queue {
   constructor() {
-    this.items = [];
+    this.first = null;
+    this.last = null;
+    this.length = 0;
   }
 
   get size() {
-    return this.items.length;
+    return this.length;
   }
 
   enqueue(element) {
-    this.items.push(element);
+    const newNode = new ListNode(element);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
   }
 
   dequeue() {
-    return this.items.shift();
+    if (!this.first) {
+      return null;
+    }
+    const temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.length--;
+    return temp.value;
   }
 }
 
