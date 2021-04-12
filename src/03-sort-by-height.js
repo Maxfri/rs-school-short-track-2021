f20eb7cefd2dcbd93371b359db30bd7c14836864
Iaroslav Silkin <maxfri1995@gmail.com>
2021-04-12 12:59:32 +0300

    feat: add solution for task 03-sort-by-height

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,20 +9,21 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
-  // const result = [];
-  // let max = -1;
-  // for (let i = 0; i < arr.length; i++) {
-  //   if (arr[i] === -1) {
-  //     result.pop(-1);
-  //   } else if (arr[i] > max) {
-  //     const index = result.indexOf(max);
-  //     result[index] = arr[i];
-  //     max = arr[i];
-  //   }
-  // }
-  // return result;
+function sortByHeight(arr) {
+  const result = [];
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      result.push(i);
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  const sortedResult = newArr.sort((a, b) => a - b);
+  for (let j = 0; j < result.length; j++) {
+    sortedResult.splice(result[j], 0, -1);
+  }
+  return sortedResult;
 }
 
 module.exports = sortByHeight;
